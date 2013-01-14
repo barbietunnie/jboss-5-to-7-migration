93dced5f89c0804ddaa4df357b43e48ec5028b44
jackwng <jackwng@gmail.com>
2013-01-14 20:36:31 +0000

    Initial add.

---
+++ MailSenderEJB/ejbModule/com/legacytojava/mailsender/ejb/MailSenderLocal.java
@@ -0,0 +1,21 @@
+package com.legacytojava.mailsender.ejb;
+import java.io.IOException;
+
+import javax.ejb.Local;
+import javax.mail.MessagingException;
+
+import com.legacytojava.jbatch.smtp.SmtpException;
+import com.legacytojava.message.bean.MessageBean;
+import com.legacytojava.message.exception.DataValidationException;
+import com.legacytojava.message.vo.emailaddr.EmailAddrVo;
+
+@Local
+public interface MailSenderLocal {
+	public void send(MessageBean msgBean) throws MessagingException, IOException, SmtpException,
+			InterruptedException, DataValidationException;
+
+	public void send(byte[] msgStream) throws MessagingException, IOException, SmtpException,
+			InterruptedException, DataValidationException;
+
+	public EmailAddrVo findByAddress(String address);
+}
