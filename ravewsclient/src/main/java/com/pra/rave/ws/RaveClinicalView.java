91b05eef59b482dd5c43a37e43814829d61a9362
jackwng@gmail.com <jackwng@gmail.com@0f8405a2-019a-c2f5-3f28-183a36256aaa>
2013-01-29 22:11:02 +0000

    Updated.

--- ravewsclient/src/main/java/com/pra/rave/ws/RaveClinicalView.java
+++ ravewsclient/src/main/java/com/pra/rave/ws/RaveClinicalView.java
@@ -1,5 +1,12 @@
 package com.pra.rave.ws;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientFactory;
 import javax.ws.rs.client.Invocation.Builder;
@@ -10,33 +17,56 @@ import org.apache.cxf.jaxrs.client.WebClient;
 import org.cdisc.ns.odm.v1.ODM;
 import org.slf4j.Logger;
 
-import com.pra.rave.xml.util.StringUtil;
 import com.pra.rave.xml.util.XmlHelper;
+import com.pra.util.Util;
 import com.pra.util.logger.LoggerHelper;
 
 public class RaveClinicalView {
 	static Logger logger = LoggerHelper.getLogger();
+	private Properties props = null;
 	
 	public static void main(String[] args) {
 		RaveClinicalView client = new RaveClinicalView();
 		try {
-			//client.getClinicalViewByFormAsString("SRF_1");
-			ODM odm = client.getClinicalViewByForm("SRF_1");
-			logger.info(StringUtil.prettyPrint(odm));
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+			java.util.Date dt = sdf.parse("2013-01-20T01:01:00");
+			client.getClinicalViewByFormAsString("ADVERSE_1", new java.sql.Timestamp(dt.getTime()));
+			//ODM odm = client.getClinicalViewByForm("SRF_1");
+			//logger.info(StringUtil.prettyPrint(odm));
 		}
 		catch (Exception e) {
 			logger.error("Exception caught", e);
 		}
 	}
 
-	public ODM getClinicalViewByForm(String formOid) {
-		WebClient client = getClinicalView(formOid);
+	private Properties loadProperties() {
+		String resourceName = "META-INF/config/ravewsclient." + Util.getEnv() + ".properties";
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		InputStream is = loader.getResourceAsStream(resourceName);
+		if (is == null) {
+			throw new RuntimeException("Resource (" + resourceName + ") missing, contact programming!");
+		}
+		Properties prop = new Properties();
+		try {
+			// XXX use InputStreamReader to load UTF-8 characters
+			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			prop.load(isr);
+		}
+		catch (IOException e) {
+			logger.error("IOException caught", e);
+			throw new RuntimeException("Failed to load resource (" + resourceName + "), contact programming!");
+		}
+		return prop;
+	}
+
+	public ODM getClinicalViewByForm(String formOid, java.sql.Timestamp startTime) {
+		WebClient client = getClinicalView(formOid, startTime);
 		ODM odm = client.get(ODM.class);
 		return odm;
 	}
 
-	public String getClinicalViewByFormAsString(String formOid) {
-		WebClient client = getClinicalView(formOid);
+	public String getClinicalViewByFormAsString(String formOid, java.sql.Timestamp startTime) {
+		WebClient client = getClinicalView(formOid, startTime);
 		String xml = client.get(String.class);
 		String xmlStr = XmlHelper.normalizeXml(xml);
 		try {
@@ -49,9 +79,32 @@ public class RaveClinicalView {
 		return xml;
 	}
 
-	private WebClient getClinicalView(String formOid) {
-		WebClient client = WebClient.create("https://hdcvcl09-018.mdsol.com");
-		client.path("RaveWebServices/studies/ABP%20980–Lilac–20120283%20(IUAT)//datasets/regular/" + formOid);
+	private WebClient getClinicalView(String formOid, java.sql.Timestamp startTime) {
+		if (props == null) {
+			props = loadProperties();
+			props.list(System.out);
+		}
+		WebClient client = WebClient.create(props.getProperty("rave.rws.url"));
+		//String path = "/RaveWebServices/studies/ABP%20980–Lilac–20120283%20(IUAT)/datasets/regular/" + formOid;
+		String path = props.getProperty("rave.studies.endpoint");
+		path += "/" + props.getProperty("rave.study.name") + "/datasets/regular/" + formOid;
+		try { // encode URI (change space to %20)
+			java.net.URI uri = new java.net.URI(
+					props.getProperty("rave.rws.scheme"),
+					props.getProperty("rave.rws.host"), path, null);
+			path = uri.getRawPath();
+		}
+		catch (URISyntaxException e) { // should never happen
+			logger.error("URISyntaxException caught", e);
+			throw new RuntimeException("URISyntaxException caught: " + e);
+		} 
+		logger.info("Path: {}", path);
+
+		client.path(path);
+		if (startTime != null) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+			client.query("start",  sdf.format(startTime));
+		}
 		String authorizationHeader = "Basic " + Base64Utility.encode("pra_rws:Sn0f@11".getBytes());
 		client.header("Authorization", authorizationHeader);
 		client.type("text/xml").accept("text/xml");
--- ravewsclient/src/main/java/com/pra/rave/ws/RaveDataLoader.java
+++ ravewsclient/src/main/java/com/pra/rave/ws/RaveDataLoader.java
@@ -3,21 +3,36 @@ package com.pra.rave.ws;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.NoResultException;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.cdisc.ns.odm.v1.ODM;
 import org.cdisc.ns.odm.v1.ODMcomplexTypeDefinitionClinicalData;
+import org.cdisc.ns.odm.v1.ODMcomplexTypeDefinitionFormData;
+import org.cdisc.ns.odm.v1.ODMcomplexTypeDefinitionItemData;
+import org.cdisc.ns.odm.v1.ODMcomplexTypeDefinitionItemGroupData;
+import org.cdisc.ns.odm.v1.ODMcomplexTypeDefinitionStudyEventData;
 import org.cdisc.ns.odm.v1.ODMcomplexTypeDefinitionSubjectData;
+import org.cdisc.ns.odm.v1.TransactionType;
 import org.slf4j.Logger;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import com.pra.rave.jpa.model.FormData;
+import com.pra.rave.jpa.model.ItemData;
+import com.pra.rave.jpa.model.ItemGroup;
 import com.pra.rave.jpa.model.Study;
+import com.pra.rave.jpa.model.StudyEvent;
 import com.pra.rave.jpa.model.StudyPK;
 import com.pra.rave.jpa.model.Subject;
+import com.pra.rave.jpa.service.FormDataService;
+import com.pra.rave.jpa.service.ItemDataService;
+import com.pra.rave.jpa.service.ItemGroupService;
+import com.pra.rave.jpa.service.StudyEventService;
 import com.pra.rave.jpa.service.StudyService;
+import com.pra.rave.jpa.service.SubjectService;
 import com.pra.rave.jpa.util.FormOid;
 import com.pra.rave.jpa.util.SpringUtil;
 import com.pra.rave.xml.util.StringUtil;
@@ -40,64 +55,301 @@ public class RaveDataLoader {
 	private PlatformTransactionManager txmgr;
 	private TransactionStatus status;
 
-	private void prepare() {
+	private void startTransaction(String tranName) {
 		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
-		def.setName("rave_service");
+		def.setName(tranName);
 		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
 		txmgr = (PlatformTransactionManager) SpringUtil.getAppContext().getBean("raveTransactionManager");
 		status = txmgr.getTransaction(def);
 	}
 
-	private void teardown() {
-		txmgr.rollback(status);
+	private void commitTransaction() {
+		txmgr.commit(status);
 	}
 
+	private StudyService studyService = null;
+	private SubjectService subjService = null;
+	private StudyEventService seService = null;
+	private FormDataService fmService = null;
+	private ItemGroupService igService = null;
+	private ItemDataService idService = null;
+	
+	boolean printOutStudyStructure = false;
+	
 	public void persistForm(String formOid, RaveClinicalView client) {
-		ODM odm = client.getClinicalViewByForm(formOid);
+		java.sql.Timestamp startTime = null;
+		ODM odm = client.getClinicalViewByForm(formOid, startTime);
 		if (odm == null) {
 			throw new IllegalArgumentException("FormOid (" + formOid + ") is invalid.");
 		}
-		prepare();
 		try {
-			StudyService studyService = (StudyService) SpringUtil.getAppContext().getBean("studyService");
-			Study study = new Study();
-			XMLGregorianCalendar cal = odm.getCreationDateTime();
-			java.sql.Timestamp loadDt = new java.sql.Timestamp(cal.getMillisecond());
-			if (FormOid.ADVERSE_1.getValue().equals(formOid)) {
-				study.setAdverse1LoadDt(loadDt);
-			}
-			else if (FormOid.AESCR_1.getValue().equals(formOid)) {
-				study.setAescr1LoadDt(loadDt);
-			}
-			else if (FormOid.SRF_1.getValue().equals(formOid)) {
-				study.setSrf1LoadDt(loadDt);
-			}
+			studyService = (StudyService) SpringUtil.getAppContext().getBean("studyService");
+			subjService = (SubjectService) SpringUtil.getAppContext().getBean("subjectService");
+			seService = (StudyEventService) SpringUtil.getAppContext().getBean("studyEventService");
+			fmService = (FormDataService) SpringUtil.getAppContext().getBean("formDataService");
+			igService = (ItemGroupService) SpringUtil.getAppContext().getBean("itemGroupService");
+			idService = (ItemDataService) SpringUtil.getAppContext().getBean("itemDataService");
+			int tran_idx = 0;
+			/*
+			 * Study
+			 */
 			for (ODMcomplexTypeDefinitionClinicalData clinical : odm.getClinicalData()) {
+				startTransaction(clinical.getStudyOID() + (tran_idx++));
 				StudyPK pk = new StudyPK(clinical.getStudyOID());
-				study.setStudyPK(pk);
-				study.setMetaDataVersionOID(clinical.getMetaDataVersionOID());
-				studyService.insert(study);
-				if (study.getSubjects()==null) {
-					List<Subject> lst = new ArrayList<Subject>();
-					study.setSubjects(lst);
-				}
-				for (ODMcomplexTypeDefinitionSubjectData subject : clinical.getSubjectData()) {
-					Subject subj = new Subject();
-					subj.setStudy(study);
-					subj.setSubjectKey(subject.getSubjectKey());
-					if (subject.getSiteRef()!=null) {
-						subj.setLocationOID(subject.getSiteRef().getLocationOID());
-					}
-					study.getSubjects().add(subj);
+				Study study = null;
+				try {
+					study = studyService.getByStudyPK(pk);
+					logger.debug("Study ({}) found, update...", pk.getStudyOID());
+					study.setMetaDataVersionOID(clinical.getMetaDataVersionOID());
+					setStudyLoadTimes(study, odm, formOid);
 					studyService.update(study);
-					//txmgr.commit(status);
 				}
-				logger.debug(StringUtil.prettyPrint(study));
+				catch (NoResultException e) {
+					logger.debug("Study ({}) not found, insert...", pk.getStudyOID());
+					study = new Study();
+					study.setStudyPK(pk);
+					study.setMetaDataVersionOID(clinical.getMetaDataVersionOID());
+					setStudyLoadTimes(study, odm, formOid);
+					studyService.insert(study);
+				}
+				/*
+				 * Subject
+				 */
+				for (ODMcomplexTypeDefinitionSubjectData subject : clinical.getSubjectData()) {
+					study = studyService.getByStudyPK(pk);
+					Subject subj = null;
+					try {
+						subj = subjService.getByPrimaryKey(pk, subject.getSubjectKey());
+						logger.debug("Subject ({}.{}) found, update...", pk.getStudyOID(), subject.getSubjectKey());
+						if (subject.getSiteRef()!=null) {
+							subj.setLocationOID(subject.getSiteRef().getLocationOID());
+						}
+						subjService.update(subj);
+					}
+					catch (NoResultException e) {
+						logger.debug("Subject ({}.{}) not found, insert...", pk.getStudyOID(), subject.getSubjectKey());
+						subj = new Subject();
+						subj.setSubjectKey(subject.getSubjectKey());
+						subj.setStudy(study);
+						if (subject.getSiteRef()!=null) {
+							subj.setLocationOID(subject.getSiteRef().getLocationOID());
+						}
+						if (study.getSubjects()==null) {
+							List<Subject> lst = new ArrayList<Subject>();
+							study.setSubjects(lst);
+						}
+						study.getSubjects().add(subj);
+						studyService.update(study);
+					}
+					subject.getStudyEventData();
+					/*
+					 * Study Event
+					 */
+					for (ODMcomplexTypeDefinitionStudyEventData studyEvent : subject.getStudyEventData()) {
+						StudyEvent se = null;
+						String logSeStr = subject.getSubjectKey() + "." + studyEvent.getStudyEventOID() + "." + studyEvent.getStudyEventRepeatKey();
+						try {
+							se = seService.getByPrimaryKey(pk,
+									subject.getSubjectKey(),
+									studyEvent.getStudyEventOID(),
+									studyEvent.getStudyEventRepeatKey());
+							logger.debug("StudyEvent ({}.{}) found...", pk.getStudyOID(), logSeStr);
+						}
+						catch (NoResultException e) {
+							logger.debug("StudyEvent ({}.{}) not found, insert...", pk.getStudyOID(), logSeStr);
+							se = new StudyEvent();
+							se.setSubject(subj);
+							se.setStudyEventOID(studyEvent.getStudyEventOID());
+							se.setStudyEventRepeatKey(studyEvent.getStudyEventRepeatKey());
+							if (subj.getStudyEvents()==null) {
+								List<StudyEvent> lst = new ArrayList<StudyEvent>();
+								subj.setStudyEvents(lst);
+							}
+							subj.getStudyEvents().add(se);
+							subjService.update(subj);
+						}
+						/*
+						 * Form Data
+						 */
+						for (ODMcomplexTypeDefinitionFormData formData : studyEvent.getFormData()) {
+							FormData fm = null;
+							String logFmStr = subject.getSubjectKey() + "."
+									+ studyEvent.getStudyEventOID() + "."
+									+ studyEvent.getStudyEventRepeatKey() + "."
+									+ formData.getFormOID() + "."
+									+ formData.getFormRepeatKey();
+							try {
+								fm = fmService.getByPrimaryKey(pk,
+										subject.getSubjectKey(),
+										studyEvent.getStudyEventOID(),
+										studyEvent.getStudyEventRepeatKey(),
+										formData.getFormOID(),
+										formData.getFormRepeatKey());
+								logger.debug("FormData ({}.{}) found...", pk.getStudyOID(), logFmStr);
+							}
+							catch (NoResultException e) {
+								logger.debug("FormData ({}.{}) not found, insert...", pk.getStudyOID(), logFmStr);
+								fm = new FormData();
+								fm.setStudyEvent(se);
+								fm.setFormOID(formData.getFormOID());
+								fm.setFormRepeatKey(formData.getFormRepeatKey());
+								if (se.getFormDataList()==null) {
+									List<FormData> lst = new ArrayList<FormData>();
+									se.setFormDataList(lst);
+								}
+								se.getFormDataList().add(fm);
+								seService.update(se);
+							}
+							/*
+							 * Item Group
+							 */
+							for (ODMcomplexTypeDefinitionItemGroupData itemGroup : formData.getItemGroupData()) {
+								TransactionType tranType = null;
+								if (startTime!=null) { // Incremental
+									tranType = itemGroup.getTransactionType();
+								}
+								ItemGroup ig = null;
+								String logIgStr = subject.getSubjectKey() + "."
+										+ studyEvent.getStudyEventOID() + "."
+										+ studyEvent.getStudyEventRepeatKey() + "."
+										+ formData.getFormOID() + "."
+										+ formData.getFormRepeatKey() + "."
+										+ itemGroup.getItemGroupOID() + "."
+										+ itemGroup.getItemGroupRepeatKey();
+								try {
+									ig = igService.getByPrimaryKey(pk,
+													subject.getSubjectKey(),
+													studyEvent.getStudyEventOID(),
+													studyEvent.getStudyEventRepeatKey(),
+													formData.getFormOID(),
+													formData.getFormRepeatKey(),
+													itemGroup.getItemGroupOID(),
+													itemGroup.getItemGroupRepeatKey());
+									logger.debug("ItemGroup ({}.{}) found...", pk.getStudyOID(), logIgStr);
+									if (TransactionType.REMOVE.equals(tranType)) {
+										igService.delete(ig); // XXX cascade delete
+										continue;
+									}
+								}
+								catch (NoResultException e) {
+									logger.debug("ItemGroup ({}.{}) not found...", pk.getStudyOID(), logIgStr);
+									if (TransactionType.REMOVE.equals(tranType)) {
+										continue; // record to be removed does not exist locally
+									}
+									ig = new ItemGroup();
+									ig.setFormData(fm);
+									ig.setItemGroupOID(itemGroup.getItemGroupOID());
+									ig.setItemGroupRepeatKey(itemGroup.getItemGroupRepeatKey());
+									if (fm.getItemGroups()==null) {
+										List<ItemGroup> lst = new ArrayList<ItemGroup>();
+										fm.setItemGroups(lst);
+									}
+									fm.getItemGroups().add(ig);
+									fmService.update(fm);
+								}
+								// get existing items from local database
+								List<ItemData> ids = idService.getByAncestorKeys(pk,
+										subject.getSubjectKey(),
+										studyEvent.getStudyEventOID(),
+										studyEvent.getStudyEventRepeatKey(),
+										formData.getFormOID(),
+										formData.getFormRepeatKey(),
+										itemGroup.getItemGroupOID(),
+										itemGroup.getItemGroupRepeatKey());
+								logger.debug("Number of items ({}) found under key: ({})", ids.size(), logIgStr);
+								/*
+								 * Item Data
+								 */
+								// loop through items received from remote site
+								for (ODMcomplexTypeDefinitionItemData itemData : itemGroup.getItemDataGroup()) {
+									// does the item exist in local database?
+									ItemData id = getByItemOid(itemData.getItemOID(), ids);
+									if (id==null) { // no, insert
+										logger.debug("Item OID ({}) not found, insert...", itemData.getItemOID());
+										if (TransactionType.REMOVE.equals(itemData.getTransactionType())) {
+											continue;
+										}
+										id = new ItemData();
+										id.setItemGroup(ig);
+										id.setItemOID(itemData.getItemOID());
+										id.setItemValue(itemData.getValue());
+										if (itemData.getIsNull()!=null) {
+											id.setIsNull(itemData.getIsNull().value());
+										}
+										if (ig.getItemDataList()==null) {
+											List<ItemData> lst = new ArrayList<ItemData>();
+											ig.setItemDataList(lst);
+										}
+										ig.getItemDataList().add(id);
+										igService.update(ig);
+									}
+									else { // yes, update
+										logger.debug("Item OID ({}) found, update...", itemData.getItemOID());
+										if (TransactionType.REMOVE.equals(itemData.getTransactionType())) {
+											id.setRemove(true);
+										}
+										else {
+											id.setItemValue(itemData.getValue());
+											if (itemData.getIsNull()!=null) {
+												id.setIsNull(itemData.getIsNull().value());
+											}
+											else {
+												id.setIsNull(null);
+											}
+											id.setRemove(false);
+											idService.update(id);
+										}
+									}
+									if (isCaseNumber(itemData.getItemOID())) {
+										// TODO
+									}
+								}
+								// remove items not present in Rave
+								for (ItemData id : ids) {
+									if (id.isRemove()) {
+										idService.delete(id);
+									}
+								}
+							}
+						}
+					}
+					logger.info("Committing changes to subject ({})...", subject.getSubjectKey());
+				}
+				if (printOutStudyStructure && tran_idx==odm.getClinicalData().size()) {
+					logger.debug(StringUtil.prettyPrint(study));
+				}
+				commitTransaction();
 			}
 		}
-		finally {
-			teardown();
+		finally { 
+			//
 		}
  	}
 
+	private boolean isCaseNumber(String itemOid) {
+		return false;
+	}
+
+	private ItemData getByItemOid(String oid, List<ItemData> list) {
+		for (ItemData id : list) {
+			if (id.getItemOID().equals(oid)) {
+				return id;
+			}
+		}
+		return null;
+	}
+
+	private void setStudyLoadTimes(Study study, ODM odm, String formOid) {
+		XMLGregorianCalendar cal = odm.getCreationDateTime();
+		java.sql.Timestamp loadDt = new java.sql.Timestamp(cal.getMillisecond());
+		if (FormOid.ADVERSE_1.getValue().equals(formOid)) {
+			study.setAdverse1LoadDt(loadDt);
+		}
+		else if (FormOid.AESCR_1.getValue().equals(formOid)) {
+			study.setAescr1LoadDt(loadDt);
+		}
+		else if (FormOid.SRF_1.getValue().equals(formOid)) {
+			study.setSrf1LoadDt(loadDt);
+		}
+	}
 }
