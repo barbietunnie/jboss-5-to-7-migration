dba4cac7a2f5cb99bd6a13458d2045e5e78b087d
jackwng <jackwng@gmail.com>
2013-02-12 03:24:47 +0000

    Updated.

--- EclipseLinkTest/src/jpa/constant/TableColumnName.java
+++ EclipseLinkTest/src/jpa/constant/TableColumnName.java
@@ -1,17 +1,21 @@
 package jpa.constant;
 
-public class TableColumnName {
+public enum TableColumnName {
 
 	// address names associated to client table columns
-	public final static String CUSTOMER_CARE_ADDR = "Customer_Care"; // ContactEmail
-	public final static String SECURITY_DEPT_ADDR = "Security_Dept";
-	public final static String RMA_DEPT_ADDR = "RMA_Dept";
-	public final static String SPAM_CONTROL_ADDR = "SPAM_Control";
-	public final static String VIRUS_CONTROL_ADDR = "Virus_Control";
-	public final static String CHALLENGE_HANDLER_ADDR = "Challenge_Handler";
+	CUSTOMER_CARE_ADDR("Customer_Care"), // ContactEmail
+	SECURITY_DEPT_ADDR("Security_Dept"),
+	RMA_DEPT_ADDR("RMA_Dept"),
+	SPAM_CONTROL_ADDR("SPAM_Control"),
+	VIRUS_CONTROL_ADDR("Virus_Control"),
+	CHALLENGE_HANDLER_ADDR("Challenge_Handler");
 
-	public final static String[] CLIENT_TABLE_EMAIL_COLUMNS = { CUSTOMER_CARE_ADDR,
-			SECURITY_DEPT_ADDR, RMA_DEPT_ADDR, SPAM_CONTROL_ADDR, VIRUS_CONTROL_ADDR,
-			CHALLENGE_HANDLER_ADDR };
-
+	private final String value;
+	private TableColumnName(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
 }
