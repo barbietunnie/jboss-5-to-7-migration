e52cd0bbda2a4fed3ac515497523e8e06b815cc8
jackwng@gmail.com <jackwng@gmail.com@0f8405a2-019a-c2f5-3f28-183a36256aaa>
2013-11-27 19:01:46 +0000

    Updated.

--- MessageCore2/src/main/java/com/es/data/preload/EnumInterface.java
+++
@@ -1,5 +0,0 @@
-package com.es.data.preload;
-
-public interface EnumInterface {
-	public static final String LF = System.getProperty("line.separator", "\n");
-}
--- MessageCore2/src/main/java/com/es/vo/comm/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlJavaTypeAdapter(value=TimestampAdapter.class,type=Timestamp.class)
-package com.es.vo.comm;
-import java.sql.Timestamp;
-
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
