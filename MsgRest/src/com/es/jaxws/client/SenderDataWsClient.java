2abf590ed00bd478866b6491d9f30b5cd60fd902
jackwng@gmail.com <jackwng@gmail.com@0f8405a2-019a-c2f5-3f28-183a36256aaa>
2015-05-01 14:07:27 +0000

    Share project "MsgRest" into "https://jboss-5-to-7-migration.googlecode.com/svn"

---
+++ MsgRest/src/com/es/jaxws/client/SenderDataWsClient.java
@@ -0,0 +1,56 @@
+package com.es.jaxws.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import javax.xml.ws.soap.SOAPFaultException;
+
+import jpa.constant.Constants;
+import jpa.util.StringUtil;
+
+import org.apache.log4j.Logger;
+
+import com.es.ejb.senderdata.SenderDataWs;
+import com.es.ejb.ws.vo.SenderDataVo;
+import com.es.tomee.util.TomeeCtxUtil;
+
+public class SenderDataWsClient {
+	protected final static Logger logger = Logger.getLogger(SenderDataWsClient.class);
+	
+	public static void main(String[] args) {
+		testSenderDataWs();
+	}
+	
+	static void testSenderDataWs() {
+		int port = TomeeCtxUtil.findHttpPort(new int[] {8181, 8080});
+		try {
+			Service service = Service.create(new URL("http://localhost:" + port + "/MsgUI2/webservices/SenderData?wsdl"),
+				new QName("http://com.es.ws.senderdata/wsdl", "SenderDataService"));
+			assertNotNull(service);
+			SenderDataWs senderDao = service.getPort(SenderDataWs.class);
+			List<SenderDataVo> list = senderDao.getAll();
+			assert(!list.isEmpty());
+			for (SenderDataVo vo : list) {
+				logger.info(StringUtil.prettyPrint(vo));
+			}
+			SenderDataVo vo = senderDao.getBySenderId(Constants.DEFAULT_SENDER_ID);
+			assertNotNull(vo);
+			try {
+				senderDao.getBySenderId("FakeSender");
+				fail();
+			}
+			catch (SOAPFaultException se) {
+				// expected
+				logger.error("SOAPFaultException caught", se);
+			}
+		}
+		catch (Exception e) {
+			logger.error("Exception caught", e);
+		}
+	}
+}
